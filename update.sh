e9097b18549e2d75beb2198e0bdd305fa5959286
Bogdan Bolbot <Bolbot2018@outlook.com>
2019-10-10 01:47:18 +0300

    also install gcc-5 and g++-5

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 sudo apt-get install gcc-4.9 g++-4.9
 sudo apt-get install build-essential
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+sudo apt-get install gcc-5 g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 70 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
 sudo apt-get install libboost-all-dev -y
