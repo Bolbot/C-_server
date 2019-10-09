f5d8e6b1337105eba7286d3a66850ed7a9d0fc13
Bogdan Bolbot <Bolbot2018@outlook.com>
2019-10-10 01:44:56 +0300

    fixed typo in update script

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-4.9 g++-4.9
-sudo upt-get install build-essential
+sudo apt-get install build-essential
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 70 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
